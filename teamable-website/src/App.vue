2ca79594f938da04ffc07c8ba1790a45b7c8b61d
dgeorgak <georgakopoulos@bryter.io>
2023-01-30 19:30:35 +0200

    cleanup with v-model and v-show

--- teamable-website/src/App.vue
+++ teamable-website/src/App.vue
@@ -1,30 +1,30 @@
 <template>
-    <div id="display-view">
+    <div v-show="!isEditMode">
         <h1>User profile</h1>
         <img :src="image" />
-        <span>Name: </span><b id="name">Dim Georgak</b>
+        <span>Name: </span><b id="name">{{ name }}</b>
         <hr />
-        <span>Email: </span><b id="email">dgeorgak@example.com</b>
+        <span>Email: </span><b id="email">{{ email }}</b>
         <hr />
-        <span>Interests: </span><b id="interests">bird watching</b>
+        <span>Interests: </span><b id="interests">{{ interests }}</b>
         <hr />
         <button @click="handleEditProfile">Edit Profile</button>
     </div>
-    <div id="edit-view">
+    <div v-show="isEditMode">
         <h1>User profile</h1>
         <img :src="image" />
         <span>Name: </span>
-        <input type="text" id="input-name" placeholder="Dim Georgak" />
+        <input type="text" id="input-name" v-model="name" />
         <hr />
         <span>Email: </span>
         <input
             type="text"
             id="input-email"
-            placeholder="dgeorgak@example.com"
+            v-model="email"
         />
         <hr />
         <span>Interests: </span>
-        <input type="text" id="input-interests" placeholder="bird watching" />
+        <input type="text" id="input-interests" v-model="interests" />
         <hr />
         <button @click="handleUpdateProfile">Update Profile</button>
     </div>
@@ -38,40 +38,18 @@ export default {
     data() {
         return {
             image: image,
+            name: "Dim Georgak",
+            email: "dgeorgak@example.com",
+            interests: "bird watching",
+            isEditMode: false
         };
     },
     methods: {
         handleEditProfile() {
-            var name = document.getElementById("name").textContent;
-            var inputName = document.getElementById("input-name");
-            inputName.value = name;
-
-            var email = document.getElementById("email").textContent;
-            var inputEmail = document.getElementById("input-email");
-            inputEmail.value = email;
-
-            var interests = document.getElementById("interests").textContent;
-            var inputInterests = document.getElementById("input-interests");
-            inputInterests.value = interests;
-
-            document.getElementById("edit-view").style.display = "block";
-            document.getElementById("display-view").style.display = "none";
+            this.isEditMode = true
         },
         handleUpdateProfile() {
-            var updatedName = document.getElementById("input-name").value;
-            var name = document.getElementById("name");
-            name.textContent = updatedName;
-
-            var updatedEmail = document.getElementById("input-email").value;
-            var email = document.getElementById("email");
-
-            var updatedInterests =
-                document.getElementById("input-interests").value;
-            var interests = document.getElementById("interests");
-            interests.textContent = updatedInterests;
-
-            document.getElementById("edit-view").style.display = "none";
-            document.getElementById("display-view").style.display = "block";
+            this.isEditMode = false
         },
     },
 };
@@ -112,7 +90,4 @@ input {
     padding: 10px;
 }
 
-#edit-view {
-    display: none;
-}
 </style>
